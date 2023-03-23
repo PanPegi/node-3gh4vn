a8eeddb621d442474b96fe6baf9d5a0c4742fae9
PanPegi <126827850+PanPegi@users.noreply.github.com>
2023-03-23 13:48:43 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import axios from 'axios';
 
 const app = express();
 app.use('/spse.html', express.static('spse.html'));
@@ -18,6 +19,8 @@ app.get('/', (req, res) => {
   res.write('<a href="/stats">Statistika</a>');
   res.write('<br>');
   res.write('<a href="/spse.html">ustavek</a>');
+  res.write('<br>');
+  res.write('<a href="/profile">profil</a>');
 
   res.write('</html>');
   res.end();
@@ -32,7 +35,22 @@ app.get('/stats', (req, res) => {
 
 app.get(`/profile`, async (req, res) => {
   const gh = await axios.get('https://api.github.com/users/panpegi');
-  res.json(gh.data.login);
+
+  res.write('<!DOCTYPE HTML>');
+  res.write('<html lang ="cs">');
+  res.write('<head>');
+  res.write('<meta charset="UTF-8">');
+  res.write('</head>');
+
+  res.write(`<h1>${gh.data.login}</h1>`);
+  res.write('<br>');
+  res.write(`<img src="${gh.data.avatar_url}"width="150px" height="150px" />`);
+  res.write('<br>');
+  res.write(`<h1>${gh.data.bio}</h1>`);
+  res.write('<br>');
+
+  res.write('</html>');
+  res.end();
 });
 
 app.listen(3000, () => {
